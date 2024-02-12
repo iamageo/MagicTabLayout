0320dd5b7a73931ebed532c470f19ba4311ccc4d
iamageo <geovani.af4@gmail.com>
2024-02-12 13:03:18 -0300

    feat: adding count

--- tablayout/src/main/java/com/iamageo/tablayout/MagicTabContent.kt
+++ tablayout/src/main/java/com/iamageo/tablayout/MagicTabContent.kt
@@ -8,7 +8,7 @@ import com.google.accompanist.pager.PagerState
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun MagicTabsContent(tabList: List<MagicTabItem>, pagerState: PagerState) {
-    HorizontalPager(state = pagerState) { page ->
+    HorizontalPager(state = pagerState, count = tabList.size) { page ->
         tabList[page].screen()
     }
 }
